cbdc650a297219b9ad3b79e2867303aad1e5cd9a
tomaShaw <tomashaw@163.com>
2017-10-09 17:49:38 +0800

    dddfd

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace netcore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("hei my name is Toma!");
+            Console.WriteLine("Hello World!");            
+        }
+    }
+}
